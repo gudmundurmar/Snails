5d0d31a869ec1948fdd3d7ace18160370480f68e
Baldur Björnsson <baldurb@applicon.is>
2013-11-06 10:07:10 +0000

    Setti inn hlutina úr skjalinu Ottarviðbót á rétta staði í kóðanm

--- Scripts/Sprite.js
+++ Scripts/Sprite.js
@@ -30,13 +30,34 @@ Sprite.prototype.drawAt = function (ctx, x, y) {
 Sprite.prototype.drawCentredAt = function (ctx, cx, cy, rotation) {
     if (rotation === undefined) rotation = 0;
     
+    
     var w = this.width,
         h = this.height;
-
+    
     ctx.save();
     ctx.translate(cx, cy);
     ctx.rotate(rotation);
     ctx.scale(this.scale, this.scale);
+    
+    // drawImage expects "top-left" coords, so we offset our destination
+    // coords accordingly, to draw our sprite centred at the origin
+    ctx.drawImage(this.image, 
+                  -w/2, -h/2);
+    
+    ctx.restore();
+};  
+
+Sprite.prototype.drawSnailCentredAt = function (ctx, cx, cy, rotation) {
+    if (rotation === undefined) rotation = 0;
+    
+    
+    var w = this.width,
+        h = this.height;
+    
+    ctx.save();
+    ctx.translate(cx, cy);
+    //ctx.rotate(rotation);
+    ctx.scale(rotation, this.scale);
     
     // drawImage expects "top-left" coords, so we offset our destination
     // coords accordingly, to draw our sprite centred at the origin
